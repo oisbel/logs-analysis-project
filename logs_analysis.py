ff3a686dbf1940f51760fd339f4a84d3045df301
vagrant <vagrant@vagrant.vm>
2017-10-02 19:47:25 +0000

    Refactoring it into a separate helper function

--- logs_analysis.py
+++ logs_analysis.py
@@ -11,47 +11,37 @@ DBNAME = "news"
 
 def popular_3_articles():
     """ Return the most popular three articles """
-    try:
-        database = psycopg2.connect(dbname=DBNAME)
-        cursor = database.cursor()
-        cursor.execute("select title, views from populars limit 3;")
-        articles = cursor.fetchall()
-        database.close()
-        return articles
-    except Exception as e:
-        print(e)
-        return []
+    statement="select title, views from populars limit 3;"
+    return fetch_data(statement)
 
 
 def popular_article_authors():
     """ Return the most popular article authors """
-    try:
-        database = psycopg2.connect(dbname=DBNAME)
-        cursor = database.cursor()
-        cursor.execute(""" select name,sum(views) as tviews
+    statement=""" select name,sum(views) as tviews
                            from populars, authors
                            where id = author
                            group by id
-                           order by tviews DESC;""")
-        articles = cursor.fetchall()
-        database.close()
-        return articles
-    except Exception as e:
-        print(e)
-        return []
+                           order by tviews DESC;"""
+    return fetch_data(statement)
 
 
 def lead_to_errors():
     """ Return the days where more than 1% of requests lead to errors  """
-    try:
-        database = psycopg2.connect(dbname=DBNAME)
-        cursor = database.cursor()
-        cursor.execute(""" select to_char(day,'FMMonth FMDD, YYYY'), percent from
+    statement=""" select to_char(day,'FMMonth FMDD, YYYY'), percent from
                            (select requests.day,
                            round(f_request*100/t_request::numeric,2) as percent
                            from requests,fails
                            where requests.day=fails.day) as result
-                           where percent >= 1;""")
+                           where percent >= 1;"""
+    return fetch_data(statement)
+
+
+def fetch_data(statement):
+    """ Connects to the database and performs the specified query (statement) """
+    try:
+        database = psycopg2.connect(dbname=DBNAME)
+        cursor = database.cursor()
+        cursor.execute(statement)
         articles = cursor.fetchall()
         database.close()
         return articles
